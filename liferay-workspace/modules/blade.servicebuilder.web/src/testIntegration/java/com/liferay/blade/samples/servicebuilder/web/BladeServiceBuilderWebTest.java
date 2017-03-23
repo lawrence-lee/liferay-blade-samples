bd4fbf2afdba415ac54e34dc3db078dc88c64fb4
Gregory Amerson <gregory.amerson@liferay.com>
2017-03-23 12:04:43 -0500

    move to blade.servicebuilder.web module

--- liferay-workspace/modules/blade.servicebuilder.web/src/testIntegration/java/com/liferay/blade/samples/servicebuilder/web/BladeServiceBuilderWebTest.java
+++ liferay-workspace/modules/blade.servicebuilder.web/src/testIntegration/java/com/liferay/blade/samples/servicebuilder/web/BladeServiceBuilderWebTest.java
@@ -14,16 +14,13 @@
  * limitations under the License.
  */
 
-package com.liferay.blade.samples.servicebuilder.test;
-
-import static org.junit.Assert.assertNotNull;
+package com.liferay.blade.samples.servicebuilder.web;
 
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 
 import org.jboss.arquillian.container.test.api.Deployment;
-import org.jboss.arquillian.container.test.api.OperateOnDeployment;
 import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.drone.api.annotation.Drone;
 import org.jboss.arquillian.junit.Arquillian;
@@ -33,11 +30,6 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-import org.osgi.framework.Bundle;
-import org.osgi.framework.BundleContext;
-import org.osgi.framework.FrameworkUtil;
 
 import com.liferay.arquillian.portal.annotation.PortalURL;
 import com.liferay.portal.kernel.exception.PortalException;
@@ -47,64 +39,32 @@ import com.liferay.portal.kernel.exception.PortalException;
  */
 @RunAsClient
 @RunWith(Arquillian.class)
-public class BladeServiceBuilderTest {
+public class BladeServiceBuilderWebTest {
 
-	@Deployment(name="portlet")
+	@Deployment
 	public static JavaArchive create() throws Exception {
 		final File jarFile = new File(System.getProperty("jarFile"));
 
 		return ShrinkWrap.createFromZipFile(JavaArchive.class, jarFile);
 	}
 
-	@Test
-	@OperateOnDeployment("portlet")
-	public void testAddPortletAction()
-		throws InterruptedException, IOException, PortalException {
-
-		_browser.get(_portlerURL.toExternalForm());
-
-		_firstParamter.clear();
-
-		_firstParamter.sendKeys("2");
-
-		_secondParameter.clear();
-
-		_secondParameter.sendKeys("3");
-
-		_add.click();
-
-		Thread.sleep(1000);
-
-		Assert.assertEquals("5", _result.getText());
-	}
 
 	@Test
-	@OperateOnDeployment("portlet")
 	public void testInstallPortlet() throws IOException, PortalException {
-		_browser.get(_portlerURL.toExternalForm());
+		String url = _portalURL.toExternalForm();
+
+		_browser.get(url);
 
 		final String bodyText = _browser.getPageSource();
 
 		Assert.assertTrue(
 			"The portlet is not well deployed",
-			bodyText.contains("Sample Portlet is deployed."));
+			bodyText.contains("Welcome to The Blade Sample Service Builder Portlet"));
 	}
-
-	@FindBy(css = "button[type=submit]")
-	private WebElement _add;
 
 	@Drone
 	private WebDriver _browser;
 
-	@FindBy(css = "input[id$='firstParameter']")
-	private WebElement _firstParamter;
-
-	@PortalURL("sample_portlet")
-	private URL _portlerURL;
-
-	@FindBy(css = "span[class='result']")
-	private WebElement _result;
-
-	@FindBy(css = "input[id$='secondParameter']")
-	private WebElement _secondParameter;
+	@PortalURL("com_liferay_blade_samples_servicebuilder_web")
+	private URL _portalURL;
 }
