ba3ef2f087697c4c5a5e67120aedd9c4b6e14078
Lawrence Lee <lawrence.lee@liferay.com>
2018-07-02 13:47:51 -0500

    fix actioncommand test

--- liferay-workspace/tests/blade.samples.test/src/testFunctional/java/com/liferay/blade/samples/portlet/actioncommand/test/BladePortletActionCommandTest.java
+++ liferay-workspace/tests/blade.samples.test/src/testFunctional/java/com/liferay/blade/samples/portlet/actioncommand/test/BladePortletActionCommandTest.java
@@ -76,10 +76,16 @@ public class BladePortletActionCommandTest {
 
 		BladeSampleFunctionalActionUtil.mouseOverClick(_webDriver, _saveButton);
 
+		Thread.sleep(1000);
+
+		_webDriver.navigate().refresh();
+
 		Assert.assertTrue(
-			"Expected Hello tester! Welcome to OSGi Hello from BLADE!, but saw " +
-				_portletBody.getText(),
-			_portletBody.getText().contains(
+			"Expected Hello tester! Welcome to OSGi Hello from BLADE!," +
+				" but saw " + _portletBody.getText(),
+			BladeSampleFunctionalActionUtil.isTextPresent(
+				_webDriver,
+				_portletBody,
 				"Hello tester! Welcome to OSGi Hello from BLADE!"));
 	}
 
