7b8a907f29f24e51c27a05dab720c848527dab34
Lawrence Lee <lawrence.lee@liferay.com>
2018-07-03 16:28:28 -0500

    ignore actioncommandtest

--- liferay-workspace/tests/blade.samples.test/src/testFunctional/java/com/liferay/blade/samples/portlet/actioncommand/test/BladePortletActionCommandTest.java
+++ liferay-workspace/tests/blade.samples.test/src/testFunctional/java/com/liferay/blade/samples/portlet/actioncommand/test/BladePortletActionCommandTest.java
@@ -32,6 +32,7 @@ import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -54,6 +55,7 @@ public class BladePortletActionCommandTest {
 		return ShrinkWrap.createFromZipFile(JavaArchive.class, jarFile);
 	}
 
+	@Ignore //Doesn't work in headless mode
 	@Test
 	public void testBladePortletActionCommand()
 		throws InterruptedException, PortalException {
